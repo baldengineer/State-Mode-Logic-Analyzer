a0ec7f7307f7153e65c64b8aebcb73dc3e318c34
baldengineer <james@baldengineer.com>
2023-11-20 16:20:35 -0600

    i2c scan from examples and pwm code on final board

--- firmware/pico/pico.c
+++ firmware/pico/pico.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/binary_info.h"
+
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
 #include "hardware/dma.h"
@@ -10,8 +12,9 @@
 
 #define CLK_IN 5
 #define TS_CNTR_CLK 2
-#define LED_PIN 25
+#define LED_PIN 3
 
+void i2c_scan();
 
 // PIO 1 of ?
 PIO pio;
@@ -224,6 +227,9 @@ inline void check_for_usb_input() {
         if ((incoming_char == '$')) {
             scan_buffer();
         }
+        if ((incoming_char == '^')) {
+            i2c_scan();
+        }
     }
 }
 
@@ -259,4 +265,51 @@ int main() {
         check_for_usb_input();
     }
     return 0;
+}
+
+// I2C reserves some addresses for special purposes. We exclude these from the scan.
+// These are any addresses of the form 000 0xxx or 111 1xxx
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+
+#define SDA_PIN 0
+#define SCL_PIN 1
+
+
+void i2c_scan() {
+    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+    // gpio_pull_up(SDA_PIN);
+    // gpio_pull_up(SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(SDA_PIN, SCL_PIN, GPIO_FUNC_I2C));
+
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
 }
